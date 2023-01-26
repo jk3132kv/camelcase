e8c77e3566015d0a9cefa7574f17d42f66a04b01
jk3132kv <114251256+jk3132kv@users.noreply.github.com>
2023-01-25 18:37:25 -0600

    Add files via upload

---
+++ camel_case.py
@@ -0,0 +1,28 @@
+def camelcase(sentence):
+    """ Convert sentence to camelCase, for example, "Display all books"
+    is converted to "displayAllBooks" """
+    title_case = sentence.title() # Uppercase first letter of each word
+    upper_camel_cased = title_case.replace(' ', '') # remove spaces
+    # Lowercase first letter, join with rest of string
+    # Slices don't produce index out of bounds errors.
+    # So this still works on empty strings, strings of length 1
+    return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
+
+def banner():
+    """ Display program name"""
+    message = 'Awesome camelcase program!!'
+    stars = '*' * len(message)
+    print(f'\n{stars} \n{message} \n{stars}\n')  
+
+def instructions():
+    print('Enter a sentence and this program will convertit to camelcase.')
+    
+def main():
+    banner()
+    instructions()
+    sentence = input('Enter your sentence: ')
+    output = camelcase(sentence)
+    print(output)
+
+if __name__ == '__main__':
+    main()
